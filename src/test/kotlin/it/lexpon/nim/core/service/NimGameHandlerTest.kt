93a7be7e21f83f4a017dac62cd67174c6abff9c7
Alex Ponomarenko <alex@lexpon.de>
2019-05-21 16:59:14 +0200

    Add tests for restarting and ending a game

--- src/test/kotlin/it/lexpon/nim/core/service/NimGameHandlerTest.kt
+++ src/test/kotlin/it/lexpon/nim/core/service/NimGameHandlerTest.kt
@@ -1,11 +1,13 @@
 package it.lexpon.nim.core.service
 
 import com.nhaarman.mockito_kotlin.whenever
-import it.lexpon.nim.core.domainobject.GameEventType.COMPUTER_MOVE
-import it.lexpon.nim.core.domainobject.GameEventType.START
+import it.lexpon.nim.core.domainobject.GameEventType.*
 import it.lexpon.nim.core.domainobject.Player.COMPUTER
 import it.lexpon.nim.core.domainobject.Player.HUMAN
+import it.lexpon.nim.core.exception.GameNotEndableException
+import it.lexpon.nim.core.exception.GameNotRestartableException
 import it.lexpon.nim.core.exception.GameNotStartableException
+import it.lexpon.nim.core.exception.NoGameException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.jupiter.api.assertThrows
@@ -69,37 +71,97 @@ class NimGameHandlerTest {
 
     @Test
     fun `should restart a running game`() {
+        // GIVEN
+        whenever(randomPlayerGenerator.determineRandomPlayer()).thenReturn(HUMAN)
+        testee.startGame()
 
+        // WHEN
+        val info = testee.reStartGame()
+
+        // THEN
+        assertThat(info).isNotNull
+        assertThat(info.gameEvents).isNotNull
+        assertThat(info.gameEvents.size).isEqualTo(1)
+        assertThat(info.gameEvents[0].gameEventType).isEqualTo(RESTART)
     }
 
     @Test
     fun `should restart a running game and make a computer move`() {
+        // GIVEN
+        whenever(randomPlayerGenerator.determineRandomPlayer()).thenReturn(COMPUTER)
+        testee.startGame()
 
+        // WHEN
+        val info = testee.reStartGame()
+
+        // THEN
+        assertThat(info).isNotNull
+        assertThat(info.gameEvents).isNotNull
+        assertThat(info.gameEvents.size).isEqualTo(2)
+        assertThat(info.gameEvents[0].gameEventType).isEqualTo(RESTART)
+        assertThat(info.gameEvents[1].gameEventType).isEqualTo(COMPUTER_MOVE)
     }
 
     @Test
     fun `should not restart a game - no game started yet`() {
-
+        // THEN
+        assertThrows<NoGameException> {
+            // WHEN
+            testee.reStartGame()
+        }
     }
 
     @Test
     fun `should not restart a game - game ended already`() {
+        // GIVEN
+        whenever(randomPlayerGenerator.determineRandomPlayer()).thenReturn(HUMAN)
+        testee.startGame()
+        testee.endGame()
 
+        // THEN
+        assertThrows<GameNotRestartableException> {
+            // WHEN
+            testee.reStartGame()
+        }
     }
 
     @Test
     fun `should end a running game`() {
+        // GIVEN
+        whenever(randomPlayerGenerator.determineRandomPlayer()).thenReturn(HUMAN)
+        testee.startGame()
 
+        // WHEN
+        val info = testee.endGame()
+
+        // THEN
+        assertThat(info).isNotNull
+        assertThat(info.gameEvents).isNotNull
+        assertThat(info.gameEvents.size).isEqualTo(1)
+        assertThat(info.gameEvents[0].gameEventType).isEqualTo(END)
     }
 
     @Test
     fun `should not end a game - game not started yet`() {
-
+        // THEN
+        assertThrows<NoGameException> {
+            // WHEN
+            testee.endGame()
+        }
     }
 
     @Test
     fun `should not end a game - game ended already`() {
+        // GIVEN
+        whenever(randomPlayerGenerator.determineRandomPlayer()).thenReturn(HUMAN)
+        testee.startGame()
+        testee.endGame()
 
+        // THEN
+        assertThrows<GameNotEndableException> {
+            // WHEN
+            testee.endGame()
+        }
     }
 
     @Test
